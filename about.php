9d69343ab238f4838621433974a22a90270e6b2a
karadag <nissa.karadag@ynov.com>
2026-02-04 18:45:33 +0100

    qui somme nous

--- about.php
+++ about.php
@@ -2,20 +2,22 @@
 $pageTitle = "Qui sommes-nous ?";
 require __DIR__ . "/includes/header.php";
 ?>
-<h1>Qui sommes-nous ?</h1>
-<p>
-  E-Courses est une plateforme de vente de cours en ligne créée dans le cadre du projet PHP (Bachelor 2).
-  Realisé par des etudiants a ynov campus Val d'Europe.
-</p>
-<p>
-  Située en France, notre plateforme vise à offrir une expérience d'apprentissage en ligne accessible et conviviale.
-</p>
-<p>
-  Notre priorité est de fournir des cours de qualité tout en garantissant la sécurité et la confidentialité des données de nos utilisateurs.
-  Avec des prix resonnable et des cours variés.
-</p>
-<p>
-  Notre objectif : proposer un catalogue, un panier, un système de comptes, et un back-office d’administration.
-  L'Admin peut editer les cours, ajoouter supprimé modifier des cours.
-</p>
+<section class="about-section">
+  <h1>Qui sommes-nous ?</h1>
+  <p>
+    E-Courses est une plateforme de vente de cours en ligne créée dans le cadre du projet PHP (Bachelor 2).
+    Realisé par des etudiants a ynov campus Val d'Europe.
+  </p>
+  <p>
+    Située en France, notre plateforme vise à offrir une expérience d'apprentissage en ligne accessible et conviviale.
+  </p>
+  <p>
+    Notre priorité est de fournir des cours de qualité tout en garantissant la sécurité et la confidentialité des données de nos utilisateurs.
+    Avec des prix resonnable et des cours variés.
+  </p>
+  <p>
+    Notre objectif : proposer un catalogue, un panier, un système de comptes, et un back-office d’administration.
+    L'Admin peut editer les cours, ajouter supprimé modifier des cours.
+  </p>
+</section>
 <?php require __DIR__ . "/includes/footer.php"; ?>
